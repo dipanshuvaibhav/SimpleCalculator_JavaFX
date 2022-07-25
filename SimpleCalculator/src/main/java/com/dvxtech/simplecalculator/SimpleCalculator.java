35b59ee45217ec389167cf42bac066c47e4dec5c
Dipanshu Vaibhav <53404726+dipanshuvaibhav@users.noreply.github.com>
2022-07-25 23:57:23 +0530

    Initial Commit

---
+++ SimpleCalculator/src/main/java/com/dvxtech/simplecalculator/SimpleCalculator.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.dvxtech.simplecalculator;
+
+/**
+ *
+ * @author dipanshuvaibhav
+ */
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
